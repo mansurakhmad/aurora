4b52529c461b0fd71c341aa5db30a0225104e7d1
mansurakhmad <mansurakhmad.051294@gmail.com>
2025-12-20 22:22:39 +0300

    added base layout

---
+++ src/app/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { MainPage } from "../pages/MainPage";
+</script>
+
+<template>
+  <MainPage />
+</template>
---
+++ src/pages/MainPage/ui/MainPage.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const name = "vue js";
+</script>
+
+<template>
+  <div class="root">I love {{ name }}</div>
+</template>
+
+<style lang="scss" scoped>
+.root {
+  background: var(--bg-primary);
+  font-size: 18px;
+  font-weight: 600;
+  color: var(--primary-600);
+}
+</style>
